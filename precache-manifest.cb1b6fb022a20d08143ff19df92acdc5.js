9f21a20ca2323b61d1d858c485cefbb78c0163d2
Ruslan Shuranov <ruslanshuranov@gmail.com>
2019-11-27 15:47:14 +0200

    Updates

---
+++ precache-manifest.cb1b6fb022a20d08143ff19df92acdc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0de83eb859657925688356bc49d6a6a",
+    "url": "/TodoListApp/index.html"
+  },
+  {
+    "revision": "fdc069dcc818c2e89586",
+    "url": "/TodoListApp/static/css/main.7c07a1e9.chunk.css"
+  },
+  {
+    "revision": "a6c454a6ab281692b999",
+    "url": "/TodoListApp/static/js/2.34fc7943.chunk.js"
+  },
+  {
+    "revision": "fdc069dcc818c2e89586",
+    "url": "/TodoListApp/static/js/main.cb1ac89d.chunk.js"
+  },
+  {
+    "revision": "32f6cd51260292d785d7",
+    "url": "/TodoListApp/static/js/runtime-main.b810164e.js"
+  }
+]);
