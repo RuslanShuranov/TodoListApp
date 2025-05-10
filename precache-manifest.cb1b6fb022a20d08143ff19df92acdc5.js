fbeea165fb39fd8664e0bcf63eb6f9f02c5d8673
Ruslan Shuranov <ruslanshuranov@gmail.com>
2025-05-10 23:30:34 +0200

    Refactor file paths in HTML and JS files for consistency

--- precache-manifest.cb1b6fb022a20d08143ff19df92acdc5.js
+++ precache-manifest.cb1b6fb022a20d08143ff19df92acdc5.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "c0de83eb859657925688356bc49d6a6a",
-    "url": "/TodoListApp/index.html"
+    "url": "/index.html"
   },
   {
     "revision": "fdc069dcc818c2e89586",
-    "url": "/TodoListApp/static/css/main.7c07a1e9.chunk.css"
+    "url": "/static/css/main.7c07a1e9.chunk.css"
   },
   {
     "revision": "a6c454a6ab281692b999",
-    "url": "/TodoListApp/static/js/2.34fc7943.chunk.js"
+    "url": "/static/js/2.34fc7943.chunk.js"
   },
   {
     "revision": "fdc069dcc818c2e89586",
-    "url": "/TodoListApp/static/js/main.cb1ac89d.chunk.js"
+    "url": "/static/js/main.cb1ac89d.chunk.js"
   },
   {
     "revision": "32f6cd51260292d785d7",
-    "url": "/TodoListApp/static/js/runtime-main.b810164e.js"
+    "url": "/static/js/runtime-main.b810164e.js"
   }
 ]);
